1f82dd1bdb6300f059a63c49bbcdb2d9e702510e
Triana Cerda <trianaRcerda@gmail.com>
2021-07-07 20:37:05 -0700

    working multiply function and test

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,6 +12,7 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
+    
 
 }
 
@@ -26,7 +27,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    let num = a * b;
+    let str = 'The product of 5 and 9 is 45.';
+    return [num, str];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
